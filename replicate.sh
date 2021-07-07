aeaec6967f098da8338171419abbc5054825affc
ToniMaciel <jaam@cin.ufpe.com>
2021-07-07 15:50:39 -0300

    Update replicate.sh

--- replicate.sh
+++ replicate.sh
@@ -5,10 +5,10 @@ export back=$PWD
 git clone https://github.com/spgroup/mergedataset
 cd mergedataset
 git checkout c8b965f
-cd mergedataset/semantic-conflicts
+cd semantic-conflicts
 python3 get_sample.py
 cd $back
 
 #Docker commands for tool executation
 docker build -t infra --build-arg DIRPATH=$PWD --build-arg DEST_CSV="mergedataset/semantic-conflicts/results_semantic_study.csv" .
-docker run -v $PWD/output:$PWD/output -v $PWD/mergedataset:$PWD/mergedataset -u="$(id -u $USER)" -it infra
+docker run -u="$(id -u $USER)" -v $PWD:$PWD/output -v $PWD/mergedataset:$PWD/mergedataset -it infra
