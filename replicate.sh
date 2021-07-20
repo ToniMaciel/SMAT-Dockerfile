d1750171e4786784550f541bf4fa83016daff220
ToniMaciel <jaam@cin.ufpe.com>
2021-07-20 17:59:06 -0300

    Update replicate.sh

--- replicate.sh
+++ replicate.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
 #Clone dataset and generate csv
 export back=$PWD
-git clone https://github.com/spgroup/mergedataset
+if [! -d "mergedataset"]; then git clone https://github.com/spgroup/mergedataset; fi
 cd mergedataset
 git checkout c8b965f
 cd semantic-conflicts
 python3 get_sample.py
 cd $back
-
 #Docker commands for tool executation
-docker build -t infra --build-arg DIRPATH=$PWD --build-arg DEST_CSV="mergedataset/semantic-conflicts/results_semantic_study.csv" .
-docker run -u="$(id -u $USER)" -v $PWD:$PWD/output -v $PWD/mergedataset:$PWD/mergedataset -it infra
+docker build -t infra --build-arg DIRPATH=$PWD --build-arg DEST_CSV="$PWD/mergedataset/semantic-conflicts/results_semantic_study.csv" .
+docker run -u="$(id -u $USER)" -v $PWD:$PWD/output -it infra
