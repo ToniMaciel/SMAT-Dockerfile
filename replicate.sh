a8c03bb591f8e5563314fdbc3d7c95c2011e6be9
José Antônio Alves Maciel <48805063+ToniMaciel@users.noreply.github.com>
2021-08-05 15:28:18 -0300

    Update replicate.sh

--- replicate.sh
+++ replicate.sh
@@ -8,5 +8,6 @@ cd semantic-conflicts
 python3 get_sample.py
 cd $back
 #Docker commands for tool executation
+mkdir output
 docker build -t infra --build-arg DIRPATH=$PWD --build-arg DEST_CSV="$PWD/mergedataset/semantic-conflicts/results_semantic_study.csv" .
-docker run -u="$(id -u $USER)" -v $PWD:$PWD/output -it infra
+docker run -u="$(id -u $USER)" -v $PWD/output:$PWD/output -v $PWD/mergedataset:$PWD/mergedataset -it infra
